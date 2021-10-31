42ea002f1b5ab22ba4669e54ac3264e88236551e
Movsar Bekaev <ext.movsar.bekaev@ef.com>
2021-10-31 19:51:57 +0300

    works well

--- InputRadioGroupEnumExample/Shared/InputRadioGroup.cs
+++ InputRadioGroupEnumExample/Shared/InputRadioGroup.cs
@@ -9,12 +9,13 @@ using System.Globalization;
 using System.Reflection;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace InputRadioGroupEnumExample.Shared
 {
     public sealed class InputRadioGroup<TEnum> : InputBase<TEnum>
     {
-        Dictionary<string, bool> CheckStatuses = new Dictionary<string, bool>();
+        private readonly Dictionary<string, bool> CheckStatuses = new Dictionary<string, bool>();
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
@@ -29,30 +30,34 @@ namespace InputRadioGroupEnumExample.Shared
             foreach (TEnum value in Enum.GetValues(enumType))
             {
                 CheckStatuses.TryAdd(value.ToString(), value.ToString() == CurrentValueAsString);
-                builder.OpenElement(4, "input");
-                builder.AddAttribute(5, "type", "radio");
-                builder.AddAttribute(6, "name", value.ToString());
-                builder.AddAttribute(7, "checked", CheckStatuses[value.ToString()]);
-                builder.AddAttribute(8, "onchange", HandleValueChange(value.ToString()));
+                builder.OpenElement(4, "div");
+
+                builder.OpenElement(5, "input");
+                builder.AddAttribute(6, "type", "radio");
+                builder.AddAttribute(7, "id", value.ToString());
+                builder.AddAttribute(8, "checked", CheckStatuses[value.ToString()]);
+                builder.AddAttribute(9, "onchange", EventCallback.Factory.Create(this, HandleValueChange(value.ToString())));
                 builder.CloseElement();
 
-                builder.OpenElement(9, "label");
-                builder.AddAttribute(10, "for", value.ToString());
-                builder.AddContent(11, GetDisplayName(value));
+                builder.OpenElement(10, "label");
+                builder.AddAttribute(11, "for", value.ToString());
+                builder.AddContent(12, GetDisplayName(value));
+                builder.CloseElement();
+
                 builder.CloseElement();
             }
 
             builder.CloseElement();
         }
 
-        EventCallback<ChangeEventArgs> HandleValueChange(string newValue) {
+        EventCallback<ChangeEventArgs> HandleValueChange(string currentRadioName) {
             var enumType = GetEnumType();
             foreach (TEnum value in Enum.GetValues(enumType))
             {
                 CheckStatuses[value.ToString()] = value.ToString() == CurrentValueAsString;
             }
 
-            return EventCallback.Factory.CreateBinder<string>(this, newValue => CurrentValueAsString = newValue, CurrentValueAsString);
+            return EventCallback.Factory.CreateBinder<string>(this, _ => CurrentValueAsString = currentRadioName, currentRadioName);
         }
         protected override void OnAfterRender(bool firstRender)
         {
